1a2feced2bd7ea2bbac244806274f7c0d0dd4cd2
Claudio <cclaudiohenriquez@gmail.com>
2022-04-18 01:55:39 -0400

    diseño

--- D&D GestorCombate/src/Main.java
+++ D&D GestorCombate/src/Main.java
@@ -10,7 +10,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ControladorGuardarPersonaje.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene s = new Scene(root);
+        s.getStylesheets().add(getClass().getResource("aplicacion.css").toExternalForm());
+
+        primaryStage.setScene(s);
+
         primaryStage.show();
     }
 
