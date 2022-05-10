f63cf62792318d642ac8d8a557e477ee31013f0c
Claudio <cclaudiohenriquez@gmail.com>
2022-05-09 23:06:15 -0400

    mouse click estadisticas

--- D&D GestorCombate/src/ControladorFormulario.java
+++ D&D GestorCombate/src/ControladorFormulario.java
@@ -90,21 +90,23 @@ public class ControladorFormulario {
                         controller.nombre.setText("Nombre Personaje: "+nombre.getText());
                         controller.jugador.setText("Nombre Jugador: "+nombreJugador.getText());
                         controller.pts.setText("PG:"+PGolpe.getText());
+
+                        // aca se crean el combatiente
+                        int ar = Integer.parseInt(armadura.getText()),pg =Integer.parseInt(PGolpe.getText())
+                                ,bi=Integer.parseInt(BIniciativa.getText()) ,fue=Integer.parseInt(Fuerza.getText())
+                                ,in=Integer.parseInt(Inteligencia.getText()),des=Integer.parseInt(destreza.getText())
+                                ,con=Integer.parseInt(constitucion.getText()),car=Integer.parseInt(carisma.getText())
+                                ,sab=Integer.parseInt(sabiduria.getText());
+                        // crear ID random
+                        int id = (char) (rnd.nextInt(94)+33); // actualizar método de obtener una ID;
+
+                        conf.setCombatiente(nombre.getText(),nombreJugador.getText(),clase.getAccessibleText(),tipoimagen.getAccessibleText(),bi,pg,ar,fue,in,des,con,car, sab,id);
+                        controller.initData(0,conf);
                         tabla.getChildren().add(b);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
-                    // aca se crean el combatiente
-                    int ar = Integer.parseInt(armadura.getText()),pg =Integer.parseInt(PGolpe.getText())
-                            ,bi=Integer.parseInt(BIniciativa.getText()) ,fue=Integer.parseInt(Fuerza.getText())
-                            ,in=Integer.parseInt(Inteligencia.getText()),des=Integer.parseInt(destreza.getText())
-                            ,con=Integer.parseInt(constitucion.getText()),car=Integer.parseInt(carisma.getText())
-                            ,sab=Integer.parseInt(sabiduria.getText());
-                    // crear ID random
-                    int id = (char) (rnd.nextInt(94)+33); // actualizar método de obtener una ID;
-
-                    conf.setCombatiente(nombre.getText(),nombreJugador.getText(),clase.getAccessibleText(),tipoimagen.getAccessibleText(),bi,pg,ar,fue,in,des,con,car, sab,id);
 
                 }
             }
