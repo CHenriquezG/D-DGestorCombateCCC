9f476851c169e5c939041a26b8b41a52e602c80b
Claudio <cclaudiohenriquez@gmail.com>
2022-05-17 19:33:55 -0400

    afecto de muerte

--- D&D GestorCombate/src/logico/Estrategia/Estrategia.java
+++ D&D GestorCombate/src/logico/Estrategia/Estrategia.java
@@ -15,8 +15,11 @@ public interface Estrategia {
         CombatienteInstancia comb = Implicado.getCombatiente();
         if(comb.getPG() <= - 10 ) { // no es estandar
             comb.setEstado("muerto");
+
         }else if(comb.getPG() <= 0){
             comb.setEstado("inconsciente");
+            comb.getGrafico().getStylesheets().removeAll(comb.getGrafico().getStylesheets());
+            comb.getGrafico().getStylesheets().add(getClass().getResource("aplicacionMuerte.css").toExternalForm());
 
         }else{
             comb.setEstado("consciente");
